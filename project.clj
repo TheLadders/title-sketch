2f4649c0e70d9e0d7504ed9169575d8926b0a165
KayShen <kshen4@me.com>
2017-01-24 15:15:35 -0500

    init release

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-time "0.12.2"]
+                 [clj-http "2.3.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [clucy "0.4.0"]])
