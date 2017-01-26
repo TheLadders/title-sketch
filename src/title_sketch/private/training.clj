4c77db6356c83b3817d95b69cb1b1f06161752ac
KayShen <kshen4@me.com>
2017-01-26 14:42:45 -0500

    added es

--- src/title_sketch/private/training.clj
+++ src/title_sketch/private/training.clj
@@ -1,6 +1,7 @@
 (ns title-sketch.private.training
   (:require [clucy.core :as clucy]
             [title-sketch.private.core :as core]
+            [title-sketch.private.elastic :as elastic]
             [util
              [util :as util]
              [io :as io]
@@ -9,6 +10,33 @@
 
 (def ^:dynamic *bespoke-title-path* "resources/data/bespoke_titles.csv")
 (def ^:dynamic *jd-title-path* "resources/data/job_title_jd.csv")
+(def ^:dynamic *elastic-mapping*
+  {:mapping
+                     {:_all {:enabled false}
+                      :properties
+                            {:job-title-id {:type "string" :index "not_analyzed"}
+                             :job-title {:type "string" :analyzer "job_analyzer"}
+                             :job-function-id {:type "string" :index "not_analyzed"}
+                             :job-function {:type "string" :index "not_analyzed"}}}
+   :settings
+                     {:analysis
+                      {:filter
+                       {:english_stop {:type "stop" :stopwords "_english_"}
+                        :english_stemmer {:type "stemmer" :language "english"}
+                        :english_possessive_stemmer
+                        {:type "stemmer" :language "possessive_english"}}
+                       :analyzer
+                       {:job_analyzer
+                        {:tokenizer "standard"
+                         :char_filter ["html_strip"]
+                         :filter
+                         ["english_possessive_stemmer"
+                          "asciifolding"
+                          "lowercase"
+                          "english_stop"
+                          "english_stemmer"]}}}}
+   :index-name "job-title"
+   :last-update-time 1484769443})
 
 (defn- convert-data
   "Uses the first row as column labels and returns a collection of hash maps"
@@ -47,3 +75,9 @@
                    (finally nil)))
          (:jd-title training-data))
     ))
+
+(defn index-elastic
+  []
+  (if (elastic/index-exists? (:index-name *elastic-mapping*))
+    (elastic/delete-index (:index-name *elastic-mapping*)))
+  (elastic/create-index "job-title" *elastic-mapping*))
