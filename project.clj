296be0b4c12cb9178776f4266c87ca3607f90f15
Paul Grech <pgrech@pgrech.laddersoffice.com>
2017-01-26 11:20:20 -0500

    Add referral hiring string utilities, dev environment, resource path, data io.

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject title-sketch "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.apache.commons/commons-lang3 "3.4"]
+                 [info.debatty/java-string-similarity "0.18"]
+                 [cheshire "5.6.3"]
+                 [clj-time "0.13.0"]]
+  ;;:resource-paths ["resources"]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
+                   :jvm-opts ["-Xmx6g"]
+                   :source-paths ["dev"]}})
